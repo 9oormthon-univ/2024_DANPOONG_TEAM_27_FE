a24c1c525b400d3aae7449c2129fc072f3fae09c
bluekmky1 <115731932+bluekmky1@users.noreply.github.com>
2024-11-18 19:42:37 +0900

    [foundation] 앱 아이콘 추가 (#9)
    * 🍱  앱 아이콘 svg 추가
    
    * 🎨 icon asset 경로 추가

--- lib/ui/common/consts/assets.dart
+++ lib/ui/common/consts/assets.dart
@@ -3,5 +3,42 @@ class Assets {
   // static const String exampleImage1 = 'assets/images/example1.png';
 
   // Icons
-  // static const String exampleIcon1 = 'assets/icons/example1.svg';
+  static const String add = 'assets/icons/add.svg';
+  static const String arrowDown = 'assets/icons/arrow_down.svg';
+  static const String arrowLeft = 'assets/icons/arrow_left.svg';
+  static const String arrowRight = 'assets/icons/arrow_right.svg';
+  static const String bookFilled = 'assets/icons/book_filled.svg';
+  static const String bookOutlined = 'assets/icons/book_outlined.svg';
+  static const String buildingFilled = 'assets/icons/building_filled.svg';
+  static const String buildingOutlined = 'assets/icons/building_outlined.svg';
+  static const String calendar = 'assets/icons/calendar.svg';
+  static const String chartFilled = 'assets/icons/chart_filled.svg';
+  static const String chartOutlined = 'assets/icons/chart_outlined.svg';
+  static const String checklist = 'assets/icons/checklist.svg';
+  static const String closeMD = 'assets/icons/close_md.svg';
+  static const String closeSM = 'assets/icons/close_sm.svg';
+  static const String delete = 'assets/icons/delete.svg';
+  static const String done = 'assets/icons/done.svg';
+  static const String dropDown = 'assets/icons/drop_down.svg';
+  static const String dumbbell = 'assets/icons/dumbbell.svg';
+  static const String edit = 'assets/icons/edit.svg';
+  static const String fortuneColored = 'assets/icons/fortune_colored.svg';
+  static const String fortuneFilled = 'assets/icons/fortune_filled.svg';
+  static const String goal = 'assets/icons/goal.svg';
+  static const String healthFilled = 'assets/icons/health_filled.svg';
+  static const String healthOutlined = 'assets/icons/health_outlined.svg';
+  static const String heartFilled = 'assets/icons/heart_filled.svg';
+  static const String heartOutlined = 'assets/icons/heart_outlined.svg';
+  static const String homeColored = 'assets/icons/home_colored.svg';
+  static const String homeOutlined = 'assets/icons/home_outlined.svg';
+  static const String note = 'assets/icons/note.svg';
+  static const String pigFilled = 'assets/icons/pig_filled.svg';
+  static const String pigOutlined = 'assets/icons/pig_outlined.svg';
+  static const String profileColored = 'assets/icons/profile_colored.svg';
+  static const String profileFilled = 'assets/icons/profile_filled.svg';
+  static const String profileOutlined = 'assets/icons/profile_outlined.svg';
+  static const String puzzle = 'assets/icons/puzzle.svg';
+  static const String sleep = 'assets/icons/sleep.svg';
+  static const String sort = 'assets/icons/sort.svg';
+  static const String verticlaDots = 'assets/icons/verticlaDots.svg';
 }
