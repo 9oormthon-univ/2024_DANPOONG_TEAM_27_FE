d540cb283dd4f2ff3151d0f612f2c688190eb4d9
doyeon0307 <eg4rang@naver.com>
2024-11-20 03:28:29 +0900

    💩 공통 텍스트 필드
    공통 텍스트 필드

--- lib/ui/common/widgets/custom_text_field.dart
+++ lib/ui/common/widgets/custom_text_field.dart
@@ -3,12 +3,6 @@ import 'package:flutter/material.dart';
 import '../../../theme/luckit_colors.dart';
 import '../../../theme/luckit_typos.dart';
 
-/// border color, text color, text size 등등이 다 달라서
-/// 이 위젯을 베이스로 사용하려고 합니다
-/// mission input field는 본 위젯을 커스텀해서 다시 만들어 뒀습니다
-///
-/// errorText는 수동으로 구현했습니다
-/// 기본 errorText를 쓰려고 보니 height 고정이 어려운 것 같아서요
 class CustomTextField extends StatelessWidget {
   final double? fieldHeight;
   final double? fieldWidth;
@@ -49,60 +43,44 @@ class CustomTextField extends StatelessWidget {
       borderSide: BorderSide.none,
     );
 
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        SizedBox(
-          height: fieldHeight,
-          width: fieldWidth,
-          child: TextField(
-            textAlign: textAlign,
-            maxLines: 1,
-            onChanged: onChanged,
-            keyboardType: textInputType,
-            decoration: InputDecoration(
-              contentPadding: const EdgeInsets.symmetric(horizontal: 14.0),
-              filled: true,
-              fillColor: fillColor ?? LuckitColors.gray10,
-              border: baseBorder,
-              enabled: enabled,
-              disabledBorder: baseBorder,
-              enabledBorder: baseBorder.copyWith(
-                borderSide: BorderSide(
-                  color: enabledBorderColor,
-                ),
-              ),
-              focusedBorder: baseBorder.copyWith(
-                borderSide: BorderSide(
-                  color: focusedBorderColor,
-                ),
-              ),
-              errorBorder: baseBorder.copyWith(
-                borderSide: const BorderSide(
-                  color: LuckitColors.error,
-                ),
-              ),
-              hintText: hintText,
-              hintStyle: hintTextStyle.copyWith()
+    return SizedBox(
+      height: fieldHeight,
+      width: fieldWidth,
+      child: TextField(
+        textAlign: textAlign,
+        maxLines: 1,
+        onChanged: onChanged,
+        keyboardType: textInputType,
+        decoration: InputDecoration(
+          contentPadding: const EdgeInsets.symmetric(horizontal: 14.0),
+          filled: true,
+          fillColor: fillColor ?? LuckitColors.gray10,
+          border: baseBorder,
+          enabled: enabled,
+          disabledBorder: baseBorder,
+          enabledBorder: baseBorder.copyWith(
+            borderSide: BorderSide(
+              color: enabledBorderColor,
             ),
-            cursorWidth: 1.0,
-            cursorColor: LuckitColors.gray80,
-            enableInteractiveSelection: false,
-            style: textStyle,
           ),
+          focusedBorder: baseBorder.copyWith(
+            borderSide: BorderSide(
+              color: focusedBorderColor,
+            ),
+          ),
+          errorBorder: baseBorder.copyWith(
+            borderSide: const BorderSide(
+              color: LuckitColors.error,
+            ),
+          ),
+          hintText: hintText,
+          hintStyle: hintTextStyle.copyWith()
         ),
-        if (errorText != null)
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8.0),
-            child: Text(
-              errorText!,
-              style: const TextStyle(
-                color: LuckitColors.error,
-                fontSize: 10.0,
-              ),
-            ),
-          ),
-      ],
+        cursorWidth: 1.0,
+        cursorColor: LuckitColors.gray80,
+        enableInteractiveSelection: false,
+        style: textStyle,
+      ),
     );
   }
 }
