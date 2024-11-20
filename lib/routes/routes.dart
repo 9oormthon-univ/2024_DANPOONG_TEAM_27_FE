fb89a5d609f2bd0f8429638a7e80afe3bfd03a7e
doyeon0307 <eg4rang@naver.com>
2024-11-20 14:23:22 +0900

    🐛 병합 에러 해결
    병합 에러 해결

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -17,7 +17,7 @@ class Routes {
     name: '/home',
     path: '/home',
   );
-https://github.com/9oormthon-univ/2024_DANPOONG_TEAM_27_FE/pull/31/conflict?name=lib%252Froutes%252Froutes.dart&ancestor_oid=2f2103582fc58e52129bf5bd31160aa3ac966705&base_oid=e20434a14b2194792dd7eb385c59e1a681d4db91&head_oid=3ec2935aabc84d6061e98425ea5e9fb86b5038df
+//https://github.com/9oormthon-univ/2024_DANPOONG_TEAM_27_FE/pull/31/conflict?name=lib%252Froutes%252Froutes.dart&ancestor_oid=2f2103582fc58e52129bf5bd31160aa3ac966705&base_oid=e20434a14b2194792dd7eb385c59e1a681d4db91&head_oid=3ec2935aabc84d6061e98425ea5e9fb86b5038df
   // 운세 페이지
   static const RouteInfo fortune = RouteInfo(
     name: '/fortune',
@@ -44,6 +44,7 @@ https://github.com/9oormthon-univ/2024_DANPOONG_TEAM_27_FE/pull/31/conflict?name
   static const RouteInfo birth = RouteInfo(
     name: '/onboarding/birth',
     path: '/onboarding/birth',
+  );
 
   // 농장
   static const RouteInfo farm = RouteInfo(
